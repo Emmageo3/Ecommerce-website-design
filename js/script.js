51a394b9fd65b8847885628c3e2d2f4538a4d874
Emmageo3 <kanfanyemma22@gmail.com>
2021-12-11 17:48:25 +0000

    swiper

--- js/script.js
+++ js/script.js
@@ -17,4 +17,36 @@ document.querySelector('#search-btn').onclick = () =>{
 window.onscroll = () =>{
     sideBar.classList.remove('active');
     searchForm.classList.remove('active');
-};
+}
+
+document.querySelectorAll('.accordion-container .accordion').forEach(accordion =>{
+    accordion.onclick = () =>{
+        accordion.classList.toggle('active');
+    }
+});
+
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    grabCursor:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    breakpoints: {
+        450: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+});
